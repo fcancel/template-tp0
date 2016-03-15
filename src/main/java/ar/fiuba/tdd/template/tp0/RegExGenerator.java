3c4cfb2c249e9ea0157ea05be1b9c2ace486229c
Fernando Cancellara <fdcancellara@gmail.com>
2016-03-15 17:25:52 -0300

    Refactoring everything

--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -2,6 +2,7 @@ package ar.fiuba.tdd.template.tp0;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Random;
 
 public class RegExGenerator {
@@ -19,7 +20,7 @@ public class RegExGenerator {
 
         ArrayList<String> listOfStringsWithMatchingRegex = new ArrayList<String>();
 
-        for (int numberOfGeneratedStrings = 0; numberOfGeneratedStrings < numberOfResults; numberOfGeneratedStrings++){
+        for (int numberOfGeneratedStrings = 0; numberOfGeneratedStrings < numberOfResults; numberOfGeneratedStrings++) {
             listOfStringsWithMatchingRegex.add(generateOneRandomStringFromRegex(regEx));
         }
 
@@ -28,41 +29,80 @@ public class RegExGenerator {
 
 
     private String generateOneRandomStringFromRegex(String regEx) {
-        if(this.maxLength < regEx.length()){
-            return "";
+        String result = "";
+        StringBuffer buffer = new StringBuffer();
+        for(int indexOfRegex = 0; indexOfRegex < regEx.length(); indexOfRegex++) {
+            if(isSet(regEx.charAt(indexOfRegex))){
+                buffer.append(generatePossibleSet(regEx, indexOfRegex));
+            }
         }
-        else{
+        return result;
+    }
+
+    private String generatePossibleSet(String regEx, int indexOfRegex) {
+        String result;
+        int firstOccurenceAt = regEx.indexOf(']',indexOfRegex);
+        if (firstOccurenceAt == -1){
+            throw new NoSuchElementException();
+        }
+        String subString = regEx.substring(indexOfRegex + 1, firstOccurenceAt);
+        //Now we have to see if it has a quantity modifier
+        if (isQuantityModifier(regEx.charAt(firstOccurenceAt + 1))){
+            result = generateStringFromQuantity(subString, firstOccurenceAt + 1);
+        }
+        else {
+            result = selectOneRandomCharFromSet(subString);
+        }
+    }
+
+    private boolean isQuantityModifier(char c) {
+        return (c == '?' || c == '+' || c == '*');
+    }
+
+
+    private boolean isSet(char c) {
+        return (c == '[');
+    }
+    /*
+    private String generateOneRandomStringFromRegex(String regEx) {
             char currentCharOfRegex;
             StringBuffer buffer = new StringBuffer();
             Random randomGenerator = new Random();
+            ArrayList<Character> charsInSet = new ArrayList<Character>();
+
             for(int indexOfChar = 0; indexOfChar < regEx.length(); indexOfChar++){
                 currentCharOfRegex = regEx.charAt(indexOfChar);
 
                 switch (currentCharOfRegex){
                     case '.':   buffer.append(randomChar());
-                                break;
+                        break;
                     case '\\':  buffer.append(regEx.charAt(indexOfChar+1));
                                 indexOfChar++;
-                                break;
+                        break;
+                    case '[':   while(regEx.charAt(indexOfChar+1) != ']'){
+                                    charsInSet.add(regEx.charAt(indexOfChar+1));
+                                    indexOfChar++;
+                                }
+                        break;
+                    case '+':   if(this.maxLength < 10);
+                        break;
                     case '?':   if(randomGenerator.nextInt(1) == 1){ //We will generate a randomness to add or not the char
                                     if(indexOfChar > 0){            //So that it will not try to access negative values
                                         buffer.append(regEx.charAt(indexOfChar-1));
                                     }
                                 }
-                                break;
+                        break;
                     default:    buffer.append(currentCharOfRegex);
-                                break;
+                        break;
                 }
             }
 
             String stringToReturn = buffer.toString();
             System.out.println(stringToReturn);
             return stringToReturn;
-        }
     }
-
-    private char randomChar(){
-
+*/
+    private char randomChar() {
         Random randomGenerator = new Random();
         return (char) (randomGenerator.nextInt(CODE_OF_MAX_NUMBER_OF_CHAR));
     }
